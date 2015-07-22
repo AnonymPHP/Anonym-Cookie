fc8cd0128f951338318b872e1e7db7f4f88f7582
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-23 02:30:26 +0300

    added use cookies class

---
+++ src/Anonym/Components/UseCookieHeaders.php
@@ -0,0 +1,73 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\Cookie;
+    use Anonym\Components\Cookie\HeadersAlreadySendedException;
+    /**
+     * Class UseCookieHeaders
+     * @package Anonym\Components\Cookie
+     */
+    class UseCookieHeaders
+    {
+        /**
+         * @var array
+         */
+        private $cookies;
+
+        /**
+         * Cookie dosyalarının atanabilmesi için sınıfı hazırlar
+         */
+        public function __construct()
+        {
+            $this->setCookies(CookieContainer::getCookies());
+        }
+
+        /**
+         * Cookie leri header olarak atar
+         *
+         * @throws HeadersAlreadySendedException
+         * @return $this
+         */
+        public function useCookies()
+        {
+
+            if (!headers_sent()) {
+                throw new HeadersAlreadySendedException(
+                '
+                 Başlıklarınız zaten gönderilmiş, cookie kullanılamaz.
+                ');
+            }
+            foreach ($this->getCookies() as $cookie) {
+                header($cookie);
+            }
+
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getCookies()
+        {
+            return $this->cookies;
+        }
+
+        /**
+         * @param mixed $cookies
+         * @return UseCookieHeaders
+         */
+        public function setCookies($cookies)
+        {
+            $this->cookies = $cookies;
+
+            return $this;
+        }
+
+
+    }
