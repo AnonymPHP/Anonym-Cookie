e29ec8bdcff245f6968a28db33ec3a205e24d20c
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-24 19:27:52 +0300

    added anonym encoder

--- src/Anonym/Components/AnonymEncode.php
+++ src/Anonym/Components/AnonymEncode.php
@@ -8,9 +8,44 @@
      */
 
     namespace Anonym\Components\Cookie;
+    use Anonym\Components\Crypt\AnonymCrypt;
+    use Anonym\Components\Crypt\SecurityKeyGenerator;
 
-
-    class AnonymEncode
+    /**
+     * Class AnonymEncode
+     * @package Anonym\Components\Cookie
+     */
+    class AnonymEncode extends AnonymCrypt implements CookieEncoderInterface
     {
+
+        /**
+         * Sınıfı başlatır ve anahtar olarak özel güvenlik anahtarını kullanır
+         *
+         * @throws \Exception
+         */
+        public function __construct(){
+            parent::__construct();
+            $this->setSecurityKey( new SecurityKeyGenerator());
+        }
+
+        /**
+         * Metni şifreler
+         *
+         * @param string $value
+         * @return string
+         */
+        public function encode($value = ''){
+            return parent::encode($value);
+        }
+
+        /**
+         * Şifrelenmiş metni çözer
+         *
+         * @param string $value
+         * @return string
+         */
+        public function decode($value = ''){
+            return parent::decode($value);
+        }
 
     }
